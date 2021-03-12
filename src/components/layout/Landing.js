6d6612fc73e0534695f6763ff0fcad01371764e8
Ramon <ramon@ramonuribe.com>
2021-03-12 12:48:38 -0700

    minor ui changes

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -6,10 +6,9 @@ const Landing = () => {
       <section className='landing'>
          <div className='dark-overlay'>
             <div className='landing-inner'>
-               <h1 className='x-large'>Developer Connector</h1>
+               <h1 className='x-large'>Titan Gram</h1>
                <p className='lead'>
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
+                  Share posts and share the love of anime with other users
                </p>
                <div className='buttons'>
                   <Link to='/register' className='btn btn-primary'>
